79136d27344039248985e55db33760957026a801
Nicholas Martini <nicho.martini@gmail.com>
2023-05-12 12:24:22 +0200

    Added Quosm font. Reworked datagrid in Customers page.

--- Calypso/Models/CustomerModel.cs
+++ Calypso/Models/CustomerModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,20 +10,26 @@ using System.Threading.Tasks;
 namespace Calypso.Models;
 public class CustomerModel
 {
-	private string firstName;
+    private ObjectId _id;
+    private string firstName;
 	private string lastName;
 	private string phone;
 	private string email;
 
-	public string FirstName
-	{
-		get { return firstName; }
-		set { firstName = value; }
-	}
+    public ObjectId Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
     public string LastName
     {
         get { return lastName; }
         set { lastName = value; }
+    }
+    public string FirstName
+    {
+        get { return firstName; }
+        set { firstName = value; }
     }
     public string Phone
     {
--- CalypsoUI/Pages/CustomersPage.xaml.cs
+++ CalypsoUI/Pages/CustomersPage.xaml.cs
@@ -1,3 +1,5 @@
+using Syncfusion.Maui.DataGrid;
+
 namespace CalypsoUI.Pages;
 
 public partial class CustomersPage : ContentPage
@@ -6,4 +8,26 @@ public partial class CustomersPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void dataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+    {
+        // Cancels the creation of the "Id" column
+        if (e.Column.MappingName == "Id")
+        {
+            e.Cancel = true;
+        }
+
+        if (e.Column.MappingName == "FirstName")
+        {
+            e.Column.HeaderText = "Nome";
+        }
+        if (e.Column.MappingName == "LastName")
+        {
+            e.Column.HeaderText = "Cognome";
+        }
+        if (e.Column.MappingName == "Phone")
+        {
+            e.Column.HeaderText = "Telefono";
+        }
+    }
 }
